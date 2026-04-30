9030adf23fae85a502d3408c0ce863a12c1a926f
Hariswar8018 <hariswarsamasi@gmail.com>
2026-04-30 18:23:16 +0530

    done NO DEBUG

--- app.py
+++ app.py
@@ -190,5 +190,5 @@ def login():
     return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
